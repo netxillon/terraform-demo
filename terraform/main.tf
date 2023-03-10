01eea8695a7f9906fbf7da1a5c63dab257ce5183
gurmukhdhillon <gurmukhsingh@intellify.com.au>
2023-03-10 22:36:39 +1100

    updated for meetup_10mar23

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,14 +8,11 @@ resource "aws_s3_bucket" "s3_demo2_bucket" {
 
 locals {
     buckets = [aws_s3_bucket.s3_demo1_bucket, aws_s3_bucket.s3_demo2_bucket]
-    
     }
-
 
 resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
   for_each = {for idx, bucket in local.buckets: idx => bucket}
-  bucket                  = bucket = each.value.id
-
+  bucket                  = each.value.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
