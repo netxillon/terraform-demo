6292b71a7b2ab3770bc447e7a4544f2a7ff9a7aa
gurmukhdhillon <gurmukhsingh@intellify.com.au>
2023-03-10 22:14:10 +1100

    updated for meetup_10mar23

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,44 +1,23 @@
-resource "aws_vpc" "apps_vpc" {
-  cidr_block           = var.demo_vpc
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "${var.org}-${var.project}-${var.environment}-apps-vpc"
-  }
+resource "aws_s3_bucket" "s3_demo1_bucket" {
+  bucket = "${var.org}-${var.environment}-demo1-zone"
 }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.apps_vpc.id
-
-  tags = {
-    Name = "${var.org}-${var.project}-${var.environment}-vpc-gw"
-  }
+resource "aws_s3_bucket" "s3_demo2_bucket" {
+  bucket = "${var.org}-${var.environment}-demo2-zone"
 }
 
-resource "aws_vpc_dhcp_options" "dhcp_options" {
-  domain_name          = var.domain_name
-  domain_name_servers  = var.domain_name_servers
-  ntp_servers          = var.ntp_servers
-
-  tags = {
-    Name = "${var.org}-${var.project}-${var.environment}"
-  }
+resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
+  bucket                  = aws_s3_bucket.s3_demo1_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
-resource "aws_vpc_dhcp_options_association" "dns_resolver" {
-  vpc_id          = aws_vpc.apps_vpc.id
-  dhcp_options_id = aws_vpc_dhcp_options.dhcp_options.id
+resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
+  bucket                  = aws_s3_bucket.s3_demo2_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
-
-resource "aws_subnet" "apps_subnet" {
-  vpc_id            = aws_vpc.apps_vpc.id
-  cidr_block        = element(var.apps_subnets, count.index)
-  availability_zone = element(var.public_availability_zones, count.index)
-  count             = length(var.apps_subnets)
-
-  tags = {
-    Name = "${var.org}-${var.project}-apps${count.index}"
-  }
-}
-
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -32,33 +32,3 @@ variable "AWS_SECRET_ACCESS_KEY" {
   type = string
   description = "AWS Secret Access key"
 }
-
-# Define variables for vpc
-
-variable "demo_vpc" {
-  type = string
-  description = "CIDR block to be used for VPC, e.g. 10.5.0.0/16"
-}
-
-variable "public_availability_zones" {
-  description = "a comma-separated list of availability zones, defaults to all AZ of the regions"
-  default     = ["us-east-2a", "us-east-2b"]
-}
-
-variable "domain_name" {
-  description = "Domain name"
-}
-
-variable "domain_name_servers" {
-  description = "List of DNS Servers"
-}
-
-variable "ntp_servers" {
-  description = "List of NTP Servers"
-}
-
-variable "apps_subnets" {
-  type = list(string)
-  description = "List of subnets for the Redshift Cluster"
-}
-
