a1f244e1d3f033833ec16e6a66667e1bf28b3677
gurmukhdhillon <gurmukhsingh@intellify.com.au>
2023-03-11 01:05:12 +1100

    created s3 bucket with error

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,8 +2,20 @@ resource "aws_s3_bucket" "s3_demo1_bucket" {
   bucket = "${var.org}-${var.environment}-demo1-zone"
 }
 
+resource "aws_s3_bucket" "s3_demo2_bucket" {
+  bucket = "${var.org}-${var.environment}-demo2-zone"
+}
+
 resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
   bucket                  = aws_s3_bucket.s3_demo1_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
+  bucket                  = aws_s3_bucket.s3_demo2_bucket.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
