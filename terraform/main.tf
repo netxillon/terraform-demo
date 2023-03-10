e43423e22f57f2660a93b913d1b11aa60e2a5bef
gurmukhdhillon <gurmukhsingh@intellify.com.au>
2023-03-11 00:57:28 +1100

    created s3 demo bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,14 @@
+resource "aws_s3_bucket" "s3_demo1_bucket" {
+  bucket = "${var.org}-${var.environment}-demo1-zone"
+}
 
-
-
+resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
+  bucket                  = aws_s3_bucket.s3_demo1_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
 
 
 
