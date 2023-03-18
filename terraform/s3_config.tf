6c341b7f7f976b9e36dd146caa5da8158b10ccd1
gurmukhdhillon <gurmukhsingh@intellify.com.au>
2023-03-18 15:35:53 +1100

    encryption

--- terraform/s3_config.tf
+++ terraform/s3_config.tf
@@ -43,3 +43,34 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
       }
 }
 }*/
+
+locals {
+  lifecycle_rules = [
+    {
+      id      = "Expiration Rule"
+      enabled = true
+      prefix  = "reports/"
+      expiration = {
+        days = 30
+      }
+    }
+  ]
+}
+
+resource "aws_s3_bucket" "bucket-name" {
+  bucket = "aws_s3_bucket.s3_demo1_bucket.name"
+  
+  dynamic "lifecycle_rule" {
+    for_each = local.lifecycle_rules
+   
+    content {
+      id      = lifecycle_rule.each.id
+      enabled = lifecycle_rule.each.enabled
+      prefix  = lifecycle_rule.each.prefix
+
+      expiration {
+        days = lifecycle_rule.each.expiration.days
+      }
+    }
+  }
+}
