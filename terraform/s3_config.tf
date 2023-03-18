5c4dcd73b51c20927fcefbe101dc6a49ae398b7c
gurmukhdhillon <gurmukhsingh@intellify.com.au>
2023-03-18 13:54:35 +1100

    encryption

---
+++ terraform/s3_config.tf
@@ -0,0 +1,44 @@
+/*variable "s3_encrypt" {
+  type = list(object({
+    internal = number
+    external = number
+    protocol = string
+  }))
+  default = [
+    {
+      internal = 8300
+      external = 8300
+      protocol = "tcp"
+    }
+  ]
+}*/
+
+/*variable "s3_encrypt" {
+  type = list(object({
+    encrypt_type = string
+  }))
+  default = [
+    {
+      encrypt_type = "AES256"
+    }
+  ]
+}*/
+
+variable "s3_encrypt" {
+  type = "list"
+  default = []
+}
+
+s3_encrypt = [{
+    enabled = true
+    apply_server_side_encryption_by_default = [{
+      sse_algorithm     = "AES256"
+    }]
+  }]
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.s3_demo1_bucket.id
+
+  rule = [ "${var.s3_encrypt}" ]
+  
+}
