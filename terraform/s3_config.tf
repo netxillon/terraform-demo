d05975e755191ff766d26661aaeac3c963baecb4
gurmukhdhillon <gurmukhsingh@intellify.com.au>
2023-03-18 14:18:59 +1100

    encryption

--- terraform/s3_config.tf
+++ terraform/s3_config.tf
@@ -29,7 +29,7 @@ variable "s3_encrypt" {
   default = [{
     apply_server_side_encryption_by_default = [{
       sse_algorithm     = "AES256"
-    }]
+      }]
   }]
 }
 
@@ -37,6 +37,6 @@ variable "s3_encrypt" {
 resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
   bucket = aws_s3_bucket.s3_demo1_bucket.id
 
-  rule = "${var.s3_encrypt}"
+  rule = [ "${var.s3_encrypt}" ]
   
 }
