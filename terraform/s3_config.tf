8dce1681f65bf3c1059cc68205e4019bce5938b7
gurmukhdhillon <gurmukhsingh@intellify.com.au>
2023-03-18 15:50:51 +1100

    encryption

--- terraform/s3_config.tf
+++ terraform/s3_config.tf
@@ -44,33 +44,21 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
 }
 }*/
 
-locals {
-  lifecycle_rules = [
-    {
-      id      = "Expiration Rule"
-      enabled = true
-      prefix  = "reports/"
-      expiration = {
-        days = 30
-      }
+resource "aws_s3_bucket_lifecycle_configuration" "this" {
+  bucket = aws_s3_bucket.s3_demo1_bucket.bucket
+  rule {
+    filter {} # workaround error
+    status = "Enabled"
+    id     = "${local.prefix}-rotation"
+    # delete if objects are older than noncurrent_days and older than n-newer_noncurrent_versions
+    noncurrent_version_expiration {
+      noncurrent_days           = 30
+      newer_noncurrent_versions = 2
     }
-  ]
-}
-
-resource "aws_s3_bucket" "bucket-name" {
-  bucket = "aws_s3_bucket.s3_demo1_bucket.name"
-  
-  dynamic "lifecycle_rule" {
-    for_each = local.lifecycle_rules
-   
-    content {
-      id      = lifecycle_rule.each.value.id
-      enabled = lifecycle_rule.each.value.enabled
-      prefix  = lifecycle_rule.each.value.prefix
-
-      expiration {
-        days = lifecycle_rule.each.expiration.value.days
-      }
+    # move to standard IA if older than 30 days for all noncurrent versions
+    noncurrent_version_transition {
+      storage_class             = "STANDARD_IA"
+      noncurrent_days           = 30
     }
   }
 }
