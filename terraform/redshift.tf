d6b78870803ab7543979e78f2ab959242aef0419
gurmukhdeloitte <111031893+gurmukhdeloitte@users.noreply.github.com>
2023-05-10 05:31:31 +1000

    updated for redshift s3 role

--- terraform/redshift.tf
+++ terraform/redshift.tf
@@ -30,6 +30,55 @@ resource "aws_iam_role_policy_attachment" "redshift_full_access_role" {
   policy_arn = "${local.managed_policy_arns_for_redshift_full_access_role[count.index]}"
 }
 
+# Export to S3
+resource "aws_iam_role" "redshift_export_role" {
+  name = "RedshiftExportToS3Role-${var.environment}"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": [
+                    "redshift.amazonaws.com",
+                    "s3.amazonaws.com"
+                ]
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "redshift_export" {
+  name = "RedshiftExportToS3-${var.environment}-policy"
+  role = aws_iam_role.redshift_export_role.id
+
+  policy = jsonencode(
+				{
+				    "Version": "2012-10-17",
+				    "Statement": [
+				        {
+				            "Effect": "Allow",
+				            "Action": [
+				                "s3:PutObject",
+				                "s3:GetObject",
+				                "redshift:ModifyClusterIamRoles",
+				                "s3:ListBucket"
+				            ],
+				            "Resource": [
+				                "arn:aws:s3:::data-${var.environment}-demo1-zone",
+				                "arn:aws:s3:::data-${var.environment}-demo1-zone/*"
+				            ]
+				        }
+				    ]
+				}
+         )
+}
+
 resource "aws_redshift_subnet_group" "redshift_subnet_group" {
   name       = "${var.org}-data-platform-${var.environment}-redshift"
   subnet_ids = "${aws_subnet.private_subnets_redshift.*.id}"
@@ -51,6 +100,6 @@ resource "aws_redshift_cluster" "redshift_cluster" {
   automated_snapshot_retention_period   = "${var.automated_snapshot_retention_period}"
   cluster_subnet_group_name             = "${aws_redshift_subnet_group.redshift_subnet_group.id}"
   vpc_security_group_ids                = ["${aws_security_group.dbt_public_vpc.id}","${aws_default_security_group.default_private_vpc.id}"]
-  iam_roles                             = [aws_iam_role.redshift_full_access_role.arn]
+  iam_roles                             = [aws_iam_role.redshift_full_access_role.arn, aws_iam_role.redshift_export_role.arn]
 }
 
