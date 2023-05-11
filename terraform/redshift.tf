9c6a9d08effed117aa77663d84f01e0261bf2503
gurmukhdeloitte <111031893+gurmukhdeloitte@users.noreply.github.com>
2023-05-11 16:05:10 +1000

    snapshot restore

--- terraform/redshift.tf
+++ terraform/redshift.tf
@@ -98,8 +98,8 @@ resource "aws_redshift_cluster" "redshift_cluster" {
   master_username                       = "${var.CLUSTER_MASTER_USERNAME}"
   master_password                       = "${var.CLUSTER_MASTER_PASSWORD}"
   publicly_accessible			              = false
-  #final_snapshot_identifier             = "${var.org}-data-platform-${var.environment}-final"
-  skip_final_snapshot                   = true
+  final_snapshot_identifier             = "${var.org}-data-platform-${var.environment}-final-snapshot"
+  skip_final_snapshot                   = false
   automated_snapshot_retention_period   = "${var.automated_snapshot_retention_period}"
   cluster_subnet_group_name             = "${aws_redshift_subnet_group.redshift_subnet_group.id}"
   vpc_security_group_ids                = ["${aws_security_group.dbt_public_vpc.id}","${aws_default_security_group.default_private_vpc.id}"]
@@ -118,7 +118,7 @@ resource "aws_redshift_cluster" "redshift_cluster_snapshot" {
   publicly_accessible			              = false
   snapshot_identifier                   = "may11snap"
   snapshot_cluster_identifier           = "${var.org}-data-platform-${var.environment}"
-  final_snapshot_identifier             = "${var.org}-data-platform-snap-${var.environment}-final"
+  final_snapshot_identifier             = "${var.org}-data-platform-snap-${var.environment}-final-snapshot"
   skip_final_snapshot                   = false
   automated_snapshot_retention_period   = "${var.automated_snapshot_retention_period}"
   cluster_subnet_group_name             = "${aws_redshift_subnet_group.redshift_subnet_group.id}"
