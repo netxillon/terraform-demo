0ed53fe9981a99d7cc93da2f88ff9adeb5b4644a
gurmukhdeloitte <111031893+gurmukhdeloitte@users.noreply.github.com>
2023-05-11 14:50:02 +1000

    snapshot restore

--- terraform/redshift.tf
+++ terraform/redshift.tf
@@ -104,3 +104,23 @@ resource "aws_redshift_cluster" "redshift_cluster" {
   vpc_security_group_ids                = ["${aws_security_group.dbt_public_vpc.id}","${aws_default_security_group.default_private_vpc.id}"]
   iam_roles                             = [aws_iam_role.redshift_full_access_role.arn, aws_iam_role.redshift_export_role.arn]
 }
+
+
+resource "aws_redshift_cluster" "redshift_cluster" {
+  count                                 = "${var.environment}" == "prod" ? 1 : 0
+  cluster_identifier                    = "${var.org}-data-snap-${var.environment}"
+  node_type                             = "${var.cluster_node_type}"
+  number_of_nodes                       = "${var.cluster_number_of_nodes}"
+  database_name                         = "sap_${var.environment}"
+  master_username                       = "${var.CLUSTER_MASTER_USERNAME}"
+  master_password                       = "${var.CLUSTER_MASTER_PASSWORD}"
+  publicly_accessible			              = false
+  snapshot_identifier                   = "may11snap"
+  snapshot_cluster_identifier           = "${var.org}-data-platform-${var.environment}"
+  final_snapshot_identifier             = "${var.org}-data-platform-${var.environment}-final"
+  skip_final_snapshot                   = false
+  automated_snapshot_retention_period   = "${var.automated_snapshot_retention_period}"
+  cluster_subnet_group_name             = "${aws_redshift_subnet_group.redshift_subnet_group.id}"
+  vpc_security_group_ids                = ["${aws_security_group.dbt_public_vpc.id}","${aws_default_security_group.default_private_vpc.id}"]
+  iam_roles                             = [aws_iam_role.redshift_full_access_role.arn, aws_iam_role.redshift_export_role.arn]
+}
